be8824e18adbcfc235a2e85932457afd872066e4
Emma Martí <emmardi@gmail.com>
2023-07-27 16:54:17 +0200

    Create mapper entities to Dto

---
+++ OrderManagementApp/Application/Adapters/OrderMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using OrderManagementApp.Application.Dtos;
+using OrderManagementApp.Domain.Entities;
+
+
+namespace OrderManagementApp.Application.Adapters
+{
+    public class OrderMapper: Profile
+    {
+        public OrderMapper()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<Customer, CustomerDto>().ReverseMap();         
+            CreateMap<CustomerAddress, CustomerAddressDto>().ReverseMap();
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<OrderLine, OrderLineDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<TaxType,TaxTypeDto>().ReverseMap();
+        }
+    }
+}
